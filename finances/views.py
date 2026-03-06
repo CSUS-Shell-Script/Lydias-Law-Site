56ed1a1d08bfe899feff4dbdddce91db6a997af8
jasoonkp <jasonprakash1214@gmail.com>
2026-03-06 00:09:42 -0800

    resolved an nondefined include

--- finances/views.py
+++ finances/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from users.views import is_admin_user
+#from users.views import is_admin_user
 from core.decorators import superuser_required
 
 import stripe
